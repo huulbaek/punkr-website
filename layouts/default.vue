4e2e232d7e031aad7c9c9f206ee9bde576f2ee65
huulbaek <huulbaek@gmail.com>
2019-02-19 19:22:15 +0100

    Name!

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@
           </v-list-tile-avatar>
 
           <v-list-tile-content>
-            <v-list-tile-title style="color: black; font-size: 22px;">notSucks</v-list-tile-title>
+            <v-list-tile-title style="color: black; font-size: 22px;">Punksy</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
