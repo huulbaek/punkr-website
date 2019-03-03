3de41d681905cbc8ee8cd9dcb44154b281f3021f
huulbaek <huulbaek@gmail.com>
2019-03-03 08:10:49 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,16 @@
         <div class="badge" style="opacity: 1; cursor: pointer"><img alt='Get it on Google Play' src='google_play_badge.png' style="width: 230px;" @click="openGooglePlay" /></div>
         <div class="badge mb-4" style="opacity: 1; cursor: pointer"><img alt='Download from App Store' src='app_store_badge.png' style="width: 200px;" @click="openAppStore" /></div>
       </div>
+      <!-- div style="">
+        <v-carousel>
+          <v-carousel-item
+            dark
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item"
+          ></v-carousel-item>
+        </v-carousel>
+      </div>-->
     </v-flex>
     <v-flex sm12 xs12 md6>
       <div style="text-align: center;">
@@ -39,6 +49,11 @@
 
 <script>
 export default {
+  data () {
+    return {
+      items: [ 'screenshot-1.png', 'screenshot-2.png', 'screenshot-3.png', 'screenshot-4.png' ]
+    }
+  },
   methods: {
     openGooglePlay () {
       window.open('https://play.google.com/store/apps/details?id=net.sikker.lodgeapp')
--- pages/privacy.vue
+++ pages/privacy.vue
@@ -2,7 +2,7 @@
   <v-layout d-flex style="margin-top: 30px; margin-left: 30px;">
     <v-flex>
       <h1>Privacy</h1>
-      <p class="title">We really do care about your privacy, promise! Read the most important points regarding your data below.</p>
+      <p class="title">We really do care about your privacy. Read the most important points regarding your data below.</p>
       <p class="title"><b class="font-weight-black">YOUR</b> personal content data (images, messages and GPS locations) are stored for <b>23 days</b> only. After that, the data is automatically deleted.</p>
       <p class="title"><b class="font-weight-black">YOUR</b> personal content in a group is removed instantly, if you choose to leave that group.</p>
       <p class="title"><b class="font-weight-black">YOUR</b> personal content is deleted instantly, if you choose to close your account.</p>
