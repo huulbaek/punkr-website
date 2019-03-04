01c87ce2b2994660f5355a2010b723a8320f0b6a
huulbaek <huulbaek@gmail.com>
2019-03-04 08:49:50 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'og:image', content: 'https//punkr.app/feature-google-play.png' },
+      { name: 'og:image', content: 'https://punkr.app/feature-google-play.png' },
       { name: 'og:type', content: 'website' },
       { name: 'og:site_name', content: 'Punkr' },
       { name: 'og:description', content: pkg.description },
