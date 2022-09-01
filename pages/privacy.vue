5f7a3d1d97aa6b9eb0236413dd1ebe2766b6ed4f
Thomas T <huulbaek@gmail.com>
2022-09-01 08:30:43 +0200

    Add contacts

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -2,16 +2,34 @@
   <v-layout d-flex style="margin-top: 30px; margin-left: 30px;">
     <v-flex>
       <h1>Privacy</h1>
-      <p class="title">We really do care about your privacy. Read the most important points regarding your data below.</p>
-      <p class="title"><b class="font-weight-black">YOUR</b> personal content data (images, messages and GPS locations) are stored for <b>23 days</b> only. After that, the data is automatically deleted.</p>
-      <p class="title"><b class="font-weight-black">YOUR</b> personal content in a group is removed instantly, if you choose to leave that group.</p>
-      <p class="title"><b class="font-weight-black">YOUR</b> personal content is deleted instantly, if you choose to close your account.</p>
-      <p class="title"><b class="font-weight-black">YOUR</b> data is shared with no 3rd party.</p>
+      <p class="title">
+        We really do care about your privacy. Read the most important points regarding your data below.
+      </p>
+      <p class="title">
+        <b class="font-weight-black">YOUR</b> personal content data (images, messages and GPS locations) are stored for <b>23 days</b> only. After that, the data is automatically deleted.
+      </p>
+      <p class="title">
+        <b class="font-weight-black">YOUR</b> personal content in a group is removed instantly, if you choose to leave that group.
+      </p>
+      <p class="title">
+        <b class="font-weight-black">YOUR</b> personal content is deleted instantly, if you choose to close your account.
+      </p>
+      <p class="title">
+        <b class="font-weight-black">YOUR</b> data is shared with no 3rd party.
+      </p>
       <h1>Security</h1>
-      <p class="title"><b class="font-weight-black">YOUR</b> data is stored in a secure location in Europe. Only a handful of people have access to the servers and the data, but if you do not trust them or the security, you should not use this app.</p>
+      <p class="title">
+        <b class="font-weight-black">YOUR</b> data is stored in a secure location in Europe. Only a handful of people have access to the servers and the data, but if you do not trust them or the security, you should not use this app.
+      </p>
       <p>Please get in touch if you have any concerns.</p>
       <h1>Background locations</h1>
-      <p class="title"><b class="font-weight-black">You can enable location updates while the app is in the background. This will save your <b>latest</b> location on our server. Only members of the same group can access the location.</p>
+      <p class="title">
+        <b class="font-weight-black">You can</b> enable location updates while the app is in the background. This will save your <b>latest</b> location on our server. Only members of the same group can access the location.
+      </p>
+      <h1>Personal contacts</h1>
+      <p class="title">
+        <b class="font-weight-black">You can</b> give the app access to your personal contacts. This will allow you to invite friends by using their email to join your group. It is not required and the app does not copy your contacts.
+      </p>
     </v-flex>
   </v-layout>
 </template>
