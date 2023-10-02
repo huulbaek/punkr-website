8828c29d47856ea1dbe96ae652724f2fd6a1539e
Thomas T <huulbaek@gmail.com>
2023-10-02 07:24:49 +0200

    Add support and contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,8 +4,8 @@
     style="margin-top: 30px; margin-left: 30px;"
   >
     <div>
-      <h1>Contact</h1>
-      <p class="title">Send an email to huulbaek@gmail.com.</p>
+      <h1>Contact and Support</h1>
+      <p class="title">Please send an email to contact@punkr.app.</p>
     </div>
   </v-layout>
 </template>
