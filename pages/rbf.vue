980d69f1fac62914a452bcbd0f1d6de21d6f5744
Thomas T <huulbaek@gmail.com>
2024-09-15 20:42:56 +0200

    Redirect

---
+++ pages/rbf.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    window.location.href = 'https://rknup.app.link/BbiBZOd5bMb'
+  }
+}
+</script>
+
+<style>
+
+</style>
